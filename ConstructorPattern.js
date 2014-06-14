e7cf1bbeff426de865e9eeb7fbee5ad7300f1679
Amit Thakkar <vigildbest@gmail.com>
2014-06-15 00:47:36 +0530

    Adding toString method to object.

--- ConstructorPattern.js
+++ ConstructorPattern.js
@@ -4,10 +4,13 @@
 function User(name, age) {
     this.name = name;
     this.age = age;
+    this.toString = function () {
+        return "Name: " + amit.name + "And Age: " + this.age;
+    };
 }
 
 var amit = new User("Amit Thakkar", 25),
     pi = new User("Shreyance", 30);
 
-console.log("Name: ", amit.name, "And Age: ", amit.age);
-console.log("Name: ", pi.name, "And Age: ", pi.age);
+console.log(amit.toString());
+console.log(pi.toString());
