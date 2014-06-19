b571cd6ae339e9afc7e9a7fe1d8d61e3379df3b5
Amit Thakkar <vigildbest@gmail.com>
2014-06-19 23:48:01 +0530

    Adding Module Pattern Demo.

---
+++ ModulePattern/ModulePattern.js
@@ -0,0 +1,49 @@
+/**
+ * Created by amit on 19/6/14.
+ */
+function User(name, age) {
+    this.name = name;
+    this.age = age;
+    this.toString = function () {
+        return "Name: " + this.name + " And Age: " + this.age;
+    };
+}
+
+var UserManagement = (function () {
+    var users = [],
+        removeAllUsers = function () {
+            users = [];
+            return "DONE";
+        };
+    return {
+        addUser: function (user) {
+            users.push(user);
+        },
+        getUsers: function () {
+            return users.slice();
+        },
+        removeAllUsers: function (password) {
+            if (password == "amit") {
+                return removeAllUsers();
+            } else {
+                return "Wrong Password";
+            }
+        }
+    };
+})();
+
+UserManagement.addUser(new User("Amit Thakkar", 25));
+UserManagement.addUser(new User("Shreyance", 30));
+var users = UserManagement.getUsers();
+users.push(new User("PI", 32));
+console.log("Local User List: ", users);
+console.log("\n\n\n==================================================================================");
+console.log("Remote User List: ", UserManagement.getUsers());
+console.log("\n\n\n==================================================================================");
+console.log("Remove Users Result with no or Wrong Password", UserManagement.removeAllUsers());
+console.log("\n\n\n==================================================================================");
+console.log("Remove Users Result with correct Password", UserManagement.removeAllUsers("amit"));
+console.log("\n\n\n==================================================================================");
+console.log("Local User List: ", users);
+console.log("\n\n\n==================================================================================");
+console.log("Remote User List: ", UserManagement.getUsers());
